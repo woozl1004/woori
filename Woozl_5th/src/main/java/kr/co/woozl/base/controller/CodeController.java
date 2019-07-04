70f477690f4dff928deeb682841b7dc635e86c02
woozl1004 <woozlzzang@naver.com>
2019-07-04 19:40:09 +0900

    190704 19:40

--- Woozl_5th/src/main/java/kr/co/woozl/base/controller/CodeController.java
+++ Woozl_5th/src/main/java/kr/co/woozl/base/controller/CodeController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
 import com.tobesoft.xplatform.data.PlatformData;
 
 import kr.co.woozl.base.sf.BaseServiceFacade;
